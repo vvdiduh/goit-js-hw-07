a6d77b513a04a6097a5826360043a82120f7f7fb
Viktor Didukh <vvdiduh@gmail.com>
2022-10-07 22:34:43 +0300

    add gallagy

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
+const markupGallery = createImgEl(galleryItems);
 
+galleryEl.insertAdjacentHTML("beforeend", markupGallery)
+
+function createImgEl(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </div>
+        `;
+        })
+        .join('');
+}
+
+const onImgClick = (event) => {
+    event.preventDefault();
+
+    if (event.target.classList.contains("gallery"))
+        return;
+
+    const imgLink = event.target.dataset.source;
+    console.log(imgLink);
+
+    const instance = basicLightbox.create(`
+    <img src="${imgLink}" width="800" height="600">`);
+
+instance.show()
+    
+};
+
+galleryEl.addEventListener('click', onImgClick);
+
+
+console.log(galleryEl);
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
+const markupGallery = createImgEl(galleryItems);
+
+galleryEl.insertAdjacentHTML("beforeend", markupGallery)
+
+function createImgEl(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+            <a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        `;
+        })
+        .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",    
+    captionDelay: 250,
+});
 
 console.log(galleryItems);
